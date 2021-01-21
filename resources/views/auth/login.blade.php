bf0083fbc4daabf133cac49b6f0141f39f533e3b
yanhf <yanhairulfirdan@gmail.com>
2021-01-21 07:51:25 +0700

    update login form for student

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,8 +12,7 @@
                             @csrf
 
                             <div class="form-group">
-                                <label for="username"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
 
                                 <div class="col-md-6">
                                     <input id="username" type="text"
@@ -30,9 +29,6 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
                                 <div class="col-md-6">
                                     <input id="password" type="password"
                                         class="form-control input-lg @error('password') is-invalid @enderror"
