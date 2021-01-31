c77a210aa580496061f32b10e629798568d5131e
yanhf <yanhairulfirdan@gmail.com>
2021-01-31 09:06:30 +0700

    create resiter and login components

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,6 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header bg-primary text-white text-bold">{{ __('Registrasi dulu yuk!') }}</div>
-
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
