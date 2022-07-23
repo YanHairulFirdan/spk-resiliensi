47c28e82378b298e0ccf51944c718e6351d1500f
yan_backend <yanhairulfirdan@gmail.com>
2022-07-23 10:18:24 +0800

    update quize seeder

--- database/seeds/userSeeder.php
+++ database/seeds/userSeeder.php
@@ -19,57 +19,58 @@ class userSeeder extends Seeder
     public $faker;
     public function run()
     {
-        $classArr = [
-            'X IPA I',
-            'X IPA II',
-            'X IPA III',
-            'X IPA IV',
-            'X IPA V',
-            'X IPA VI',
-        ];
-        $this->faker =  Factory::create();
-        foreach ($classArr as $key => $class) {
-            for ($usercount = 0; $usercount < 30; $usercount++) {
-                $user = User::create([
-                    'name' => $this->faker->name,
-                    'username' => $this->faker->unique()->userName,
-                    'email' => $this->faker->unique()->safeEmail,
-                    'email_verified_at' => now(),
-                    'class' => $class,
-                    'phoneNumber' => $this->faker->phoneNumber,
-                    'password' => Hash::make($this->faker->password), // password
-                    'remember_token' => Str::random(10),
-                ]);
-                App\Score::create([
-                    'user_id' => $user->id,
-                    'regulasi_emosi' => rand(10, 100),
-                    'pengendalian_impuls' => rand(10, 100),
-                    'optimis' => rand(10, 100),
-                    'percaya_diri' => rand(10, 100),
-                    'analisis_kausal' => rand(10, 100),
-                    'empati' => rand(10, 100),
-                    'reaching_out' => rand(10, 100)
-                ]);
-                Answear::create([
-                    'user_id' => $user->id,
-                    'answear_1' => $this->faker->realText(),
-                    'answear_2' => $this->faker->realText(),
-                    'answear_3' => $this->faker->realText(),
-                    'answear_4' => $this->faker->realText(),
-                    'answear_5' => $this->faker->realText(),
-                    'answear_6' => $this->faker->realText()
-                ]);
-            }
-        }
-        User::create([
+        // $classArr = [
+        //     'X IPA I',
+        //     'X IPA II',
+        //     'X IPA III',
+        //     'X IPA IV',
+        //     'X IPA V',
+        //     'X IPA VI',
+        // ];
+        // $this->faker =  Factory::create();
+        // foreach ($classArr as $key => $class) {
+        //     for ($usercount = 0; $usercount < 30; $usercount++) {
+        //         $user = User::create([
+        //             'name' => $this->faker->name,
+        //             'username' => $this->faker->unique()->userName,
+        //             'email' => $this->faker->unique()->safeEmail,
+        //             'email_verified_at' => now(),
+        //             'class' => $class,
+        //             'phoneNumber' => $this->faker->phoneNumber,
+        //             'password' => Hash::make($this->faker->password), // password
+        //             'remember_token' => Str::random(10),
+        //         ]);
+        //         App\Score::create([
+        //             'user_id' => $user->id,
+        //             'regulasi_emosi' => rand(10, 100),
+        //             'pengendalian_impuls' => rand(10, 100),
+        //             'optimis' => rand(10, 100),
+        //             'percaya_diri' => rand(10, 100),
+        //             'analisis_kausal' => rand(10, 100),
+        //             'empati' => rand(10, 100),
+        //             'reaching_out' => rand(10, 100)
+        //         ]);
+        //         Answear::create([
+        //             'user_id' => $user->id,
+        //             'answear_1' => $this->faker->realText(),
+        //             'answear_2' => $this->faker->realText(),
+        //             'answear_3' => $this->faker->realText(),
+        //             'answear_4' => $this->faker->realText(),
+        //             'answear_5' => $this->faker->realText(),
+        //             'answear_6' => $this->faker->realText()
+        //         ]);
+        //     }
+        // }
+        User::query()->firstOrCreate([
             'username' => 'admin',
             "password" => Hash::make('admin123pass'),
             "role" => 'admin'
         ]);
-        User::create([
-            'name' => 'king',
-            'username' => 'king',
-            "password" => Hash::make('admin123pass'),
+
+        User::query()->firstOrCreate([
+            'username' => 'user',
+            'name' => 'user',
+            "password" => Hash::make('user123pass'),
             "role" => ''
         ]);
     }
