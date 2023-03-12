85fd03cb72df5692be3ec068049eae07af239f7e
yan_backend <yanhairulfirdan@gmail.com>
2023-03-12 09:34:33 +0800

    refactoring quiz controller

--- app/Http/Controllers/Quizcontroller.php
+++ app/Http/Controllers/Quizcontroller.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Answear;
 use App\Aspect;
+use App\Http\Requests\QuizRequest;
 use App\Quisioner;
 use App\Score;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator as FacadesValidator;
-
+use Illuminate\Support\Str;
 
 class Quizcontroller extends Controller
 {
@@ -30,7 +32,10 @@ class Quizcontroller extends Controller
     public function index()
     {
         $aspectsArr = $this->aspectsArr;
-        $aspects =  Aspect::with(['statements'])->get();
+        $aspects =  Aspect::query()
+            ->with(['statements'])
+            ->withCount(['statements'])
+            ->get();
         $options = [
             'Sangat setuju',
             'Setuju',
@@ -45,54 +50,18 @@ class Quizcontroller extends Controller
         ];
         return view('kuisioners.kuisioner', compact(['aspects', 'aspectsArr', 'options', 'skor']));
     }
-    public function saveQuiz(Request $request)
+    public function saveQuiz(QuizRequest $request)
     {
-        $this->scoreCalculation($request->except(['_token']));
-        return redirect('/hasil');
-    }
-    public function motivationForm()
-    {
-        $questions = Quisioner::get();
-        return view('kuisioners.motivasi', compact('questions'));
-    }
-    public function savemotivationForm(Request $request)
-    {
-        // dd($request->except('_token'));
-        $answearValidator = FacadesValidator::make(
-            $request->except('_token'),
-            [
-                'answear_1' => 'required|min:8',
-                'answear_2' => 'required|min:8',
-                'answear_3' => 'required|min:8',
-                'answear_4' => 'required|min:8',
-                'answear_5' => 'required|min:8',
-                'answear_6' => 'required|min:8',
-            ],
-            [
-                'required' => 'harap isi bidang ini',
-                'min' => 'jawaban anda terlalu pendek, jumlah minimal karakter untuk jawaban ini adalah 8 karakter'
-            ]
-        )->validate();
-        Answear::updateOrCreate(['user_id' => (int)auth()->user()->id], $request->except('_token'));
+        $this->scoreCalculation($request);
 
-        return redirect('/kuisioner');
+        return redirect('/hasil');
     }
 
     public function result()
     {
         $labelChart = Aspect::get()->pluck('aspect');
-        $scores = [];
-        $score =  Score::where('user_id', auth()->user()->id)->get()->toArray();
-        if (!$score) {
-            return redirect('/');
-        }
-        $show = true;
-        foreach ($this->aspectsArr as $key => $aspect) {
-            if (array_key_exists($aspect, $score[0])) {
-                $scores[$key] = $score[0][$aspect];
-            }
-        }
-        $scores = collect($scores);
+        $scores = auth()->user()->scores->pluck('score');
+
         $icons = collect([
             'low' => 'far fa-frown-open fa-2x text-danger',
             'good' => 'far fa-grin-alt fa-2x text-primary',
@@ -104,36 +73,43 @@ class Quizcontroller extends Controller
         return view('kuisioners.result', compact(['scores', 'icons', 'labels', 'labelChart']));
     }
 
-    public function scoreCalculation($request)
+    public function scoreCalculation(QuizRequest $request)
     {
-        $aspectGroup = [];
-        $final_score = 0;
-        $count = 0;
-        foreach ($this->aspectsArr as $key => $singleAspect) {
-            foreach ($request as $parentkey => $inputvalue) {
-                if (preg_match('/' . $singleAspect . '/', $parentkey)) {
-                    if (!isset($aspectGroup[$singleAspect])) {
-                        $aspectGroup[$singleAspect] = 0;
-                    }
-                    $aspectGroup[$singleAspect] += $inputvalue;
-                    $count++;
-                }
-            }
-            $totalAspect = 5 * $count;
-            $aspectGroup[$singleAspect] = ($aspectGroup[$singleAspect] / $totalAspect) * 100;
-            $count = 0;
-        }
-        $final_score = ($final_score / count($request)) * 100;
+        $aspectScores = collect($request->except(['_token']));
+        $aspectScores = $aspectScores->map(function ($scores) {
+            $scores = collect($scores);
+            $totalScore = $scores->reduce(function ($total, $score) {
+                return $total + (int)$score;
+            });
 
-        Score::updateOrCreate(['user_id' => auth()->id()], [
-            'pengendalian_impuls' => $aspectGroup['pengendalian_impuls'],
-            'regulasi_emosi' => $aspectGroup['regulasi_emosi'],
-            'optimis' => $aspectGroup['optimis'],
-            'percaya_diri' => $aspectGroup['percaya_diri'],
-            'analisis_kausal' => $aspectGroup['analisis_kausal'],
-            'empati' => $aspectGroup['empati'],
-            'reaching_out' => $aspectGroup['reaching_out'],
-            'final_score' => $final_score
-        ]);
+            $aspectTotalScore = 5 * $scores->count();
+            $finalScore = ($totalScore / $aspectTotalScore) * 100;
+
+            return $finalScore;
+        });
+
+        $aspects = Aspect::get(['id', 'aspect']);
+        DB::beginTransaction();
+
+        $scoreStored = [];
+
+        try {
+            $aspects->each(function (Aspect $aspect) use ($aspectScores, &$scoreStored) {
+                $scoreStored[] = Score::query()
+                    ->updateOrCreate(
+                        [
+                            'user_id' => auth()->id(),
+                            'aspect_id' => $aspect->id,
+                        ], 
+                        [ 'score' => $aspectScores[Str::snake($aspect->aspect)] ]
+                    );
+            });
+
+            DB::commit();    
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            
+            throw $th;
+        }
     }
 }
